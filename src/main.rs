ce293d9070d10795d331b73b25187cec467c3473
Wojciech Wais <feniks65@users.noreply.github.com>
2022-01-28 10:44:12 +0100

    add Weak reference implementation
    Weak references are needed in this case because without them, callbacks will be hanging in the callback_manager even though they won't be needed anymore in other modules. Weak references will preserve callbacks up until there won't be any reference to those callbacks in other modules.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,56 @@
-use std::boxed::Box;
+use std::rc::{Rc, Weak};
 
-type BoxedCallback = Box<dyn Fn(i32)>;
+type WeakCallback = Weak<dyn Fn(i32)>;
 
 struct CallbackManager {
-    callbacks: Vec<BoxedCallback>,
+    callbacks: Vec<WeakCallback>,
 }
 
 impl CallbackManager {
-    fn add<C>(&mut self, callback: C)
+    fn add<C>(&mut self, callback: C) -> Rc<dyn Fn(i32)>
     where
         C: Fn(i32) + 'static,
     {
-        self.callbacks.push(Box::new(callback));
+        let shared_ref_callback = Rc::new(callback);
+        let return_handle = shared_ref_callback.clone();
+        let weak_ref_callback = Rc::downgrade(&shared_ref_callback);
+        self.callbacks.push(weak_ref_callback);
+        return return_handle;
     }
 
     fn run_all(&mut self, arg: i32) {
+        // clear all hanging callbacks
+        let mut callbacks_for_removal = vec![];
+        for (pos, callback) in self.callbacks.iter().enumerate() {
+            let rc_callback = callback.upgrade();
+            if rc_callback.is_some() == false {
+                callbacks_for_removal.push(pos);
+            }
+        }
+
+        for index in callbacks_for_removal {
+            self.callbacks.remove(index);
+        }
+
+        // fire callbacks
         for (pos, callback) in self.callbacks.iter().enumerate() {
             println!("position {}", pos);
-            callback(arg);
+            let rc_callback = callback.upgrade().unwrap();
+            rc_callback(arg);
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello");
     let mut callback_manager = CallbackManager { callbacks: vec![] };
     let expensive_closure = |num| {
         let _ = num + 1;
         println!("Expensive callback closure called with arg {}", num);
         return ();
     };
+    /*let _closure_handle = */
     callback_manager.add(expensive_closure);
     callback_manager.run_all(3);
-    println!("Bye, world!");
+    println!("Bye");
 }
